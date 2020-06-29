4c316e67eb55439d36886c2f5525e82bda6710a5
LovEver_Rv <yongweili.cn@gmail.com>
2020-06-29 16:02:53 +0800

    fix load func

--- easyMark/__init__.py
+++ easyMark/__init__.py
@@ -1,9 +1,12 @@
 from .easyMark import EasyMarker
 
+# list dependencies
+dependencies = ['mcBasicLib']
+
 
 def load(logger, core):
     # Function "load" is required by mana9er-core.
     from os import path
     config_file = path.join(core.root_dir, 'easyMark', 'config.json')
     save_file = path.join(core.root_dir, 'easyMark', 'easyMark-saves.json')
-    EasyMarker(logger, core, config_file, save_file)
+    return EasyMarker(logger, core, config_file, save_file)
